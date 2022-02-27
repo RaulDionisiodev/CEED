f8d89427329b0cac5f7c28ade63d7d8bef50264b
Raul Dionisio <rauldionisiosh@gmail.com>
2022-02-27 20:13:17 -0300

    Organizing components and styles

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./estilo.css"
+
+class FormularioCadastro extends Component {
+    render() {
+        return (
+            <form className="form-cadastro">
+                <input type='text' placeholder='Título' className="form-cadastro_input"></input>
+                <textarea rows={15} className="form-cadastro_input" placeholder='Escreva sua nota...' />
+                <button className="form-cadastro_input form-cadastro_submit" >
+                    Criar nota
+                </button>
+            </form>
+        );
+    }
+}
+
+export default FormularioCadastro;
+
