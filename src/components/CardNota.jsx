a96b7815cf2dd91da205f5dd37f1ee923466d183
Raul Dionisio <rauldionisiosh@gmail.com>
2022-02-27 13:14:04 -0300

    adding class components

---
+++ src/components/CardNota.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class CardNota extends Component {
+
+    render() {
+        return (
+            <section>
+                <header>
+                    Título
+                </header>
+                <p>Escreva sua nota...</p>
+            </section>
+        );
+    }
+}
+
+export default CardNota;
