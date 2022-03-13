0be198dfe2daf6acbedab708d5d2633d01bdd429
Raul Dionisio <rauldionisiosh@gmail.com>
2022-03-13 20:04:14 -0300

    starting to handle category

--- src/components/ListaDeCategorias/ListaDeCategoria.jsx
+++ src/components/ListaDeCategorias/ListaDeCategoria.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 
 class ListaDeCategoria extends Component {
+    _handleEventoInput(e){
+        if(e.key === "Enter"){
+            console.log("adicionar catgoria")
+        }
+    }
     render() {
         return (
-            <section >
-                <ul>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
+            <section className='lista-categorias' >
+                <ul className='lista-categorias_lista'>
+                    <li className='lista-categorias_item'>Categorias</li>
+                    <li className='lista-categorias_item'>Categorias</li>
+                    <li className='lista-categorias_item'>Categorias</li>
+                    <li className='lista-categorias_item'>Categorias</li>
 
-                    <input type="text" name="" id="" />
+                    <input type="text" className='lista-categorias_input'
+                        placeholder='Adicionar categoria' 
+                        onKeyUp={this._handleEventoInput.bind(this)}/>
                 </ul>
             </section>
         )
